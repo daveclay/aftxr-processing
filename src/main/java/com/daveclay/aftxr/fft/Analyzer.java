1b47740c03fe00809aa34dfbf371b092ad2f813c
Dave Clay <dclay@invaio.com>
2024-04-20 10:51:40 -0700

    Refactor out Analyzer

---
+++ src/main/java/com/daveclay/aftxr/fft/Analyzer.java
@@ -0,0 +1,150 @@
+package com.daveclay.aftxr.fft;
+
+import processing.core.PApplet;
+import processing.sound.FFT;
+import processing.sound.SoundFile;
+
+public class Analyzer {
+
+    public static class Options {
+        PApplet parent;
+        String soundFile;
+        int bands = 4096;
+        float smoothingFactor = 0.7f;
+        int numberOfLowBandsToSkip = 8;
+        int millisecondsToHoldHit = 200;
+        float lowPowerThreshold = .1f;
+
+        private Options(PApplet sketch) {
+            this.parent = sketch;
+        }
+
+        public static Options forSketch(PApplet sketch) {
+            return new Options(sketch);
+        }
+
+        public Options soundFile(String soundFile) {
+            this.soundFile = soundFile;
+            return this;
+        }
+
+        /**
+         * Define how many FFT bands to use (this needs to be a power of two)
+         */
+        public Options bands(int bands) {
+            this.bands = bands;
+            return this;
+        }
+
+        /**
+         * Define a smoothing factor which determines how much the spectrums of consecutive
+         * points in time should be combined to create a smoother visualisation of the spectrum.
+         * A smoothing factor of 1.0 means no smoothing (only the data from the newest analysis
+         * is rendered), decrease the factor down towards 0.0 to have the visualisation update
+         * more slowly, which is easier on the eye.
+         */
+        public Options smoothingFactor(float smoothingFactor) {
+            this.smoothingFactor = smoothingFactor;
+            return this;
+        }
+
+        public Options numberOfLowBandsToSkip(int numberOfLowBandsToSkip) {
+            this.numberOfLowBandsToSkip = numberOfLowBandsToSkip;
+            return this;
+        }
+
+        public Options millisecondsToHoldHit(int millisecondsToHoldHit) {
+            this.millisecondsToHoldHit = millisecondsToHoldHit;
+            return this;
+        }
+
+        public Options lowPowerThreshold(float lowPowerThreshold) {
+            this.lowPowerThreshold = lowPowerThreshold;
+            return this;
+        }
+
+        public Analyzer buildAnalyzer() {
+            return new Analyzer(this);
+        }
+    }
+
+    private final PApplet parent;
+    private final SoundFile soundFile;
+    private final FFT fft;
+    private final int bands;
+    private final float smoothingFactor;
+    private final float millisecondsToHoldHit;
+    private final float lowPowerThreshold;
+
+    // calculated
+    private long lastHitTime = 0;
+
+    // public state
+    public final float[] summedBandValues;
+    public final int numberOfLowBandsToSkip;
+
+    // public calculated values
+    public long timeSinceHit = -1;
+    public boolean lowPassTriggered = false;
+    public boolean lowPassHold = false;
+    public float lowPowerSum = 0f;
+
+    public Analyzer(Options analyzerOptions) {
+        // Load and play a soundfile and loop it.
+        this.parent = analyzerOptions.parent;
+
+        this.soundFile = new SoundFile(parent, analyzerOptions.soundFile);
+        soundFile.loop();
+
+        this.bands = analyzerOptions.bands;
+
+        // Create the FFT analyzer and connect the playing soundfile to it.
+        this.fft = new FFT(parent, bands);
+        fft.input(soundFile);
+
+        this.summedBandValues = new float[bands];
+
+        this.smoothingFactor = analyzerOptions.smoothingFactor;
+        this.numberOfLowBandsToSkip = analyzerOptions.numberOfLowBandsToSkip;
+        this.millisecondsToHoldHit = analyzerOptions.millisecondsToHoldHit;
+        this.lowPowerThreshold = analyzerOptions.lowPowerThreshold;
+    }
+
+    public void update() {
+        lowPassTriggered = false;
+
+        fft.analyze();
+        calculateLowPowerSum();
+
+        if (lowPassHold) {
+            timeSinceHit = System.currentTimeMillis() - lastHitTime;
+            if (timeSinceHit > millisecondsToHoldHit && lowPowerSum < lowPowerThreshold) {
+                lastHitTime = 0;
+                timeSinceHit = -1;
+                lowPassTriggered = false;
+                lowPassHold = false;
+            }
+        } else {
+            if (lowPowerSum >= lowPowerThreshold) {
+                long now = System.currentTimeMillis();
+                lowPassTriggered = true;
+                lowPassHold = true;
+                lastHitTime = now;
+                timeSinceHit = 0;
+            }
+        }
+    }
+
+    private void calculateLowPowerSum() {
+        for (int i = 0; i < bands; i++) {
+            // Smooth the FFT spectrum data by smoothing factor
+            summedBandValues[i] += (fft.spectrum[i] - summedBandValues[i]) * smoothingFactor;
+
+            // TODO: low pass threshold
+            if (i > numberOfLowBandsToSkip + 3 && i < numberOfLowBandsToSkip + 10) {
+                lowPowerSum += summedBandValues[i];
+            }
+        }
+        lowPowerSum = lowPowerSum / 7f;
+    }
+}
--- src/main/java/com/daveclay/aftxr/fft/Driver1.java
+++ src/main/java/com/daveclay/aftxr/fft/Driver1.java
@@ -2,7 +2,6 @@ package com.daveclay.aftxr.fft;
 
 import processing.core.PApplet;
 import processing.opengl.PShader;
-import processing.sound.*;
 
 public class Driver1 extends PApplet {
 
@@ -10,40 +9,17 @@ public class Driver1 extends PApplet {
         new Driver1().runSketch();
     }
 
-    // Declare the sound source and FFT analyzer variables
-    SoundFile soundFile;
-    FFT fft;
-    BeatDetector beatDetector;
-
-    // Define how many FFT bands to use (this needs to be a power of two)
-    int bands = 4096;
-
-    // Define a smoothing factor which determines how much the spectrums of consecutive
-    // points in time should be combined to create a smoother visualisation of the spectrum.
-    // A smoothing factor of 1.0 means no smoothing (only the data from the newest analysis
-    // is rendered), decrease the factor down towards 0.0 to have the visualisation update
-    // more slowly, which is easier on the eye.
-    float smoothingFactor = 0.7f;
-
     // Create a vector to store the smoothed spectrum data in
-    float[] sum = new float[bands];
     int bandsToDisplay = 600;
-    int numberOfLowBandsToSkip = 8;
 
-    // Variables for drawing the spectrum:
-    // Declare a scaling factor for adjusting the height of the rectangles
-    float scale = 3;
     // Declare a drawing variable for calculating the width of the
     int barWidth;
-    float lowPowerSum = 0f;
 
-    int spectrographHeight = 400;
-    boolean hit = false;
+    Analyzer analyzer;
 
     int heightGrowthAmount = 0;
     int widthGrowthAmount = 0;
     boolean growthDirection = true;
-    long lastHitTime = 0;
 
     PShader blur;
 
@@ -52,6 +28,10 @@ public class Driver1 extends PApplet {
     }
 
     public void setup() {
+        analyzer = Analyzer.Options.forSketch(this)
+                .soundFile("Prometheus.wav")
+                .buildAnalyzer();
+
         colorMode(HSB, 1, 1, 1, 1);
         background(0);
         blur = loadShader("sepBlur.glsl");
@@ -61,54 +41,25 @@ public class Driver1 extends PApplet {
 
         // Calculate the width of the rects depending on how many bands we have
         barWidth = (int) (width/((float) bandsToDisplay));
-
-        // Load and play a soundfile and loop it.
-        soundFile = new SoundFile(this, "Shut Our Eyes.wav");
-        soundFile.loop();
-
-        // Create the FFT analyzer and connect the playing soundfile to it.
-        fft = new FFT(this, bands);
-        fft.input(soundFile);
-
-        beatDetector = new BeatDetector(this);
-        beatDetector.input(soundFile);
-        beatDetector.sensitivity(20);
     }
 
     public void draw() {
-        // Perform the analysis
-        fft.analyze();
+        analyzer.update();
+        boolean hit = analyzer.lowPassHold;
+
         heightGrowthAmount = growthDirection ? heightGrowthAmount + 1 : heightGrowthAmount - 1;
-
-        for (int i = 0; i < bands; i++) {
-            // Smooth the FFT spectrum data by smoothing factor
-            sum[i] += (fft.spectrum[i] - sum[i]) * smoothingFactor;
-
-            if (i > numberOfLowBandsToSkip + 3 && i < numberOfLowBandsToSkip + 10) {
-                lowPowerSum += sum[i];
-            }
-        }
-        lowPowerSum = lowPowerSum / 7f;
-
         background(0);
+
         if (hit) {
-            long timeSinceHit = System.currentTimeMillis() - lastHitTime;
-            float lerp = lerp(5, 20, timeSinceHit / 200f);
-            if (timeSinceHit > 300 && lowPowerSum < .1) {
-                lastHitTime = 0;
-                hit = false;
-            }
-            //background(.0f, 1f, .1f);
-            blur.set("blurSize", (int)lerp);
-            blur.set("sigma", lerp);
-        } else {
-            if (lowPowerSum > .1) {
-                long now = System.currentTimeMillis();
-                hit = true;
+            if (analyzer.lowPassTriggered) {
                 heightGrowthAmount = 0;
                 widthGrowthAmount = 0;
-                lastHitTime = now;
             }
+
+            float lerp = lerp(5, 20, analyzer.timeSinceHit / 200f);
+            background(.0f, 1f, .1f);
+            blur.set("blurSize", (int)lerp);
+            blur.set("sigma", lerp);
         }
 
         noStroke();
@@ -118,7 +69,7 @@ public class Driver1 extends PApplet {
         float hueOffset = hit ? .01f : 0;
 
         for (int i = 0; i < bandsToDisplay; i++) {
-            float power = sum[i + numberOfLowBandsToSkip];
+            float power = analyzer.summedBandValues[i + analyzer.numberOfLowBandsToSkip];
 
             float hue = (power * .45f) + hueOffset;
             float saturation = 1 - (power * 10);
@@ -135,7 +86,7 @@ public class Driver1 extends PApplet {
             rect(x, 30, barWidth + widthGrowthAmount, barHeight);
             rect((width - x), 800 - barHeight, barWidth + widthGrowthAmount, barHeight);
 
-            if (sum[i] > 0.01 && !hit) {
+            if (analyzer.summedBandValues[i + analyzer.numberOfLowBandsToSkip] > 0.01 && !hit) {
                 textSize(11);
                 text(power, x - 5, power * 500 + (height / 2f));
             }
@@ -151,6 +102,6 @@ public class Driver1 extends PApplet {
             filter(blur);
         }
 
-        text(lowPowerSum, 50, 600);
+        text(analyzer.lowPowerSum, 50, 600);
     }
 }
